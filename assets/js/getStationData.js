b16ba51e6c85998999ad8b0452099a5718edfe3b
Niklas227 <77329334+Niklas227@users.noreply.github.com>
2021-06-14 09:44:59 +0200

    details are now working with XML, but html generation still missing

--- assets/js/getStationData.js
+++ assets/js/getStationData.js
@@ -1,45 +1,39 @@
-//TODO fix for XML
 
-function get_url_param( name )
-{
-    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
 
-    var regexS = "[\\?&amp;]"+name+"=([^&amp;#]*)";
-    var regex = new RegExp( regexS );
-    var results = regex.exec( window.location.href );
+function get_url_param(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
 
-    if ( results == null )
+    var regexS = "[\\?&amp;]" + name + "=([^&amp;#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+
+    if (results == null)
         return "";
     else
         return results[1];
 }
 
 let stationName = get_url_param('station')
-let html="";
+let html = "";
 
-fetch('./assets/json/station1.json')
-    .then(res => res.json())
-    .then(data => {
-        markArr = []
-        for (let i=0; i<data.length;i++){
-            if (data[i].STATION===stationName){
-                console.log(data[i].DATA)
-                html+=" <p>"+stationName+"</p><table>\n" +
-                    "  <tr>\n" +
-                    "    <th>DATE</th>\n" +
-                    "    <th>DP01</th>\n" +
-                    "  </tr>\n" +
-                    "  <tr>\n" +
-                    "    <td>"+data[i].DATA[0].DATE+"</td>\n" +
-                    "    <td>"+data[i].DATA[0].DP01+"</td>\n" +
-                    "  </tr>\n" +
-                    "  <tr>\n" +
-                    "    <td>"+data[i].DATA[1].DATE+"</td>\n" +
-                    "    <td>"+data[i].DATA[1].DP01+"</td>\n" +
-                    "  </tr>\n" +
-                    "</table> "
-                document.getElementById("content").innerHTML = html
-            }
+var xhr = new XMLHttpRequest();
+xhr.open('GET', './assets/xml/stations.xml');
+xhr.overrideMimeType('text/xml');
+xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        console.log(xhr.responseXML)
+        details(xhr.responseXML);
+    }
+};
+
+xhr.send();
+
+function details(xml) {
+    var stations = xml.getElementsByTagName("station");
+    for (let i = 0; i < stations.length; i++) {
+        if (stations[i].getElementsByTagName("name")[0].childNodes[0].nodeValue === stationName) {
+           console.log(stations[i].getElementsByTagName("name")[0].childNodes[0].nodeValue)
+            break;
         }
-    })
-    .catch(err => console.error(err));
+    }
+}
