70629dc309900c7e6996db616b19a1ca849b51dd
Reichmann-M <46574997+Reichmann-M@users.noreply.github.com>
2021-06-21 15:43:44 +0200

    🚨 Fixed ESLint for station data fetching

--- stations/getData.js
+++ stations/getData.js
@@ -1,59 +1,60 @@
 const fs = require("fs");
-var request = require("request");
-var rp = require("request-promise");
+const rp = require("request-promise");
 
 
-const german_station_id_list = JSON.parse(fs.readFileSync("german_station_id_list.json"));
+const germanStationIdList = JSON.parse(fs.readFileSync("german_station_id_list.json"));
 
 
 async function main() {
-	console.time('fetching')
+	console.time("fetching");
 	
 	//request lat, lon, name and write into xml
-	const uriPart = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&startDate=2020-01-01&endDate=2020-12-31&includeStationLocation=1&includeStationName=true&format=json&stations="
-	let stationJSON = [];
-	let errorStations = []
-
-	const request_count = german_station_id_list.length;
-	const cooldownMs = 3000;
+	const uriPart = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&startDate=2020-01-01&endDate=2020-12-31&includeStationLocation=1&includeStationName=true&format=json&stations=",
+		stationJSON = [],
+		errorStations = [],
+		requestCount = germanStationIdList.length,
+		cooldownMs = 3000;
 	let counter = 0;
 	myInterval = setInterval(async () => {
 
-		if (counter == request_count) {
-			fs.writeFileSync('stationData.json', JSON.stringify(stationJSON))
-			fs.writeFileSync('errorStationIDs.json', JSON.stringify(errorStations))
-			console.timeEnd('fetching')
-			clearInterval(myInterval)
+		if (counter === requestCount) {
+			fs.writeFileSync("stationData.json", JSON.stringify(stationJSON));
+			fs.writeFileSync("errorStationIDs.json", JSON.stringify(errorStations));
+			console.timeEnd("fetching");
+			clearInterval(myInterval);
 		}
 
-		let station_id = german_station_id_list[counter]
-		console.log(`[${(counter / request_count*100).toFixed(1)}/100%] Requesting station data for: ${station_id}, ETA: ${((((request_count-counter) * cooldownMs) /1000) / 60).toFixed(1)} mins`);
+		const stationId = germanStationIdList[counter];
+		console.log(`[${(counter / requestCount * 100).toFixed(1)}/100%] Requesting station data for: ${stationId}, ETA: ${((((requestCount - counter) * cooldownMs) / 1000) / 60).toFixed(1)} mins`);
 		
+		let passed = false;
+		let response;
 		try {
-			var response = await rp(uriPart + station_id, { json: true });
+			response = await rp(uriPart + stationId, { json: true });
+			passed = true;
 		} catch (error) {
-			errorStations.push(station_id)
+			errorStations.push(stationId);
 		}
 		
-		try {
-			stationJSON.push({
-				id: response[0].STATION,
-				name: response[0].NAME,
-				lat: response[0].LATITUDE,
-				lon: response[0].LONGITUDE
-			})
-		} catch (error) {
-			console.log(`🚨 Some attributes for ${station_id} not available. --> Skipping this station`)
+		if (passed) {
+			try {
+				stationJSON.push({
+					id: response[0].STATION,
+					name: response[0].NAME,
+					lat: response[0].LATITUDE,
+					lon: response[0].LONGITUDE
+				});
+			} catch (error) {
+				console.log(`🚨 Some attributes for ${stationId} not available. --> Skipping this station`);
+			}
 		}
 
-		fs.writeFileSync('stationData.json', JSON.stringify(stationJSON))
-		fs.writeFileSync('errorStationIDs.json', JSON.stringify(errorStations))
+		fs.writeFileSync("stationData.json", JSON.stringify(stationJSON));
+		fs.writeFileSync("errorStationIDs.json", JSON.stringify(errorStations));
 
 		counter++;
-	}, cooldownMs)
+	}, cooldownMs);
 
 }
 
-main()
-
-
+main();
