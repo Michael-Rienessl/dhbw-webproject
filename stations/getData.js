0f8cae99f48bba356c6a1950842c3b8070b90af2
Reichmann-M <46574997+Reichmann-M@users.noreply.github.com>
2021-06-21 13:09:42 +0200

    ✨ Added complete data gathering of German Stations with 3sec delay to begin with

--- stations/getData.js
+++ stations/getData.js
@@ -3,28 +3,42 @@ var request = require("request");
 var rp = require("request-promise");
 
 
-const station_id_list = JSON.parse(fs.readFileSync("german_station_id_list.json"));
+const german_station_id_list = JSON.parse(fs.readFileSync("german_station_id_list.json"));
 
 
 async function main() {
 	//request lat, lon, name and write into xml
 	const uriPart = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&startDate=2020-01-01&endDate=2020-01-01&includeStationLocation=1&includeStationName=true&format=json&stations="
 	let stationJSON = [];
-	
-	
-	let station_id = 'GM000003342'
-	console.log(`Requesting station data for: ${station_id}`)
-	const response = await rp(uriPart + station_id, { json: true });
-	
-	stationJSON.push({
-		id: response[0].STATION,
-		name: response[0].NAME,
-		lat: response[0].LATITUDE,
-		lon: response[0].LONGITUDE
-	})
 
-	fs.writeFileSync('stationData.json', JSON.stringify(stationJSON))
+	let counter = 0;
+	myInterval = setInterval(async () => {
+
+		if (counter == german_station_id_list.length) {
+			fs.writeFileSync('stationData.json', JSON.stringify(stationJSON))
+			clearInterval(myInterval)
+		}
+
+		let station_id = german_station_id_list[counter]
+		console.log(`Requesting station data for: ${station_id}`);
+		var response = await rp(uriPart + station_id, { json: true });
+
+		try {
+			stationJSON.push({
+				id: response[0].STATION,
+				name: response[0].NAME,
+				lat: response[0].LATITUDE,
+				lon: response[0].LONGITUDE
+			})
+		} catch (error) {
+			console.log(`🚨 Some attributes for ${station_id} not available. --> Skipping this station`)
+		}
+
+		counter++;
+	}, 3000)
 }
 
 
 main()
+
+
