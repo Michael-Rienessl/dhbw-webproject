0622d73de33f7a151bb7841e59d00f3eb058aaaa
Reichmann-M <46574997+Reichmann-M@users.noreply.github.com>
2021-06-21 12:44:39 +0200

    :recycle: Refactored Gathering of station data

--- stations/getData.js
+++ stations/getData.js
@@ -4,30 +4,27 @@ var rp = require("request-promise");
 
 
 const station_id_list = JSON.parse(fs.readFileSync("german_station_id_list.json"));
-let stationJSON = [];
 
 
-//request lat, lon, name and write into xml
-var options = {
-	uri: "https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&startDate=2020-01-01&endDate=2020-01-01&stations=" + "GM000010147" + "&includeStationLocation=1&includeStationName=true&format=json",
-	json: true // Automatically parses the JSON string in the response
-};
-
-rp(options)
-	.then(function (response) {
-		console.log(response)
-		//get name, id, lat, lon and save to json
-		var dataset = {
-			id: response[0].STATION,
-			name: response[0].NAME,
-			lat: response[0].LATITUDE,
-			lon: response[0].LONGITUDE
-		};
-		console.log(dataset);
-		stationJSON.push(dataset);
+async function main() {
+	//request lat, lon, name and write into xml
+	const uriPart = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&startDate=2020-01-01&endDate=2020-01-01&includeStationLocation=1&includeStationName=true&format=json&stations="
+	let stationJSON = [];
+	
+	
+	let station_id = 'GM000003342'
+	console.log(`Requesting station data for: ${station_id}`)
+	const response = await rp(uriPart + station_id, { json: true });
+	
+	stationJSON.push({
+		id: response[0].STATION,
+		name: response[0].NAME,
+		lat: response[0].LATITUDE,
+		lon: response[0].LONGITUDE
 	})
-	.catch(function (err) {
-		// delete id?
-	});
 
-fs.writeFileSync('station.json', JSON.stringify(stationJSON))
+	fs.writeFileSync('stationData.json', JSON.stringify(stationJSON))
+}
+
+
+main()
