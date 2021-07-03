473d39508f2ddc90b6e639f046d1884121c21195
Reichmann-M <46574997+Reichmann-M@users.noreply.github.com>
2021-07-03 17:39:43 +0200

    🔨 Fixed build script +🔊 Added more output in script

--- build.bat
+++ build.bat
@@ -1,1 +1,10 @@
-if not exist "./dist" mkdir "./dist" && tsc && copy .\\js\\getStationData.js .\\dist\\ && copy .\\src\\map.js .\\dist\\
+@echo off
+if not exist "./dist" mkdir "./dist"
+echo "------> Created dist folder ✅"
+
+call tsc
+echo "------> Compiled typescript ✅"
+
+copy .\js\getStationData.js .\dist\
+copy .\src\map.js .\dist\
+echo "------> Copied src scripts into dist folder ✅"
